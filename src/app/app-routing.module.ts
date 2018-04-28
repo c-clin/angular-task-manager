2dec27919703fd55be292b9b81d8385208dfcf65
Catherine Lin <catherine.lin.93@gmail.com>
2018-04-28 10:01:47 -0700

    Added routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { TodosComponent } from './todos/todos/todos.component';
 import { NgModule } from '@angular/core';
+import { ActiveComponent } from './todos/todos/active/active.component';
+import { CompletedComponent } from './todos/todos/completed/completed.component';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const appRoutes: Routes = [
+  { path: '', redirectTo: 'all', pathMatch: 'full' },
+  { path: 'all', component: TodosComponent},
+  { path: 'active', component: ActiveComponent },
+  { path: 'completed', component: CompletedComponent }
+];
 
-]
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos/todos.component';
-
+import { ActiveComponent } from './todos/todos/active/active.component';
+import { CompletedComponent } from './todos/todos/completed/completed.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavigationComponent } from './todos/todos/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    ActiveComponent,
+    CompletedComponent,
+    NavigationComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
