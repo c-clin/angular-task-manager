245c0985cbb6265a7828e4310aa322efb5763dfd
Catherine Lin <catherine.lin.93@gmail.com>
2018-05-03 01:21:17 -0700

    updated README

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# Angular ToDo App
+# Angular Task Manager
 
 ### What it is
-This is a todo app made with Angular using all of the frameworks core functions.
+* This is a todo app made with Angular using all of the frameworks core functionalities.
+ * Services & dependency injection
+ * Routers
+ * Forms 
+ * Observables
+ * Custome pipe
+ * Http Calls
+
+* There is a sign up page and a log in function for the user to be authenticated.
+* Users are required to be authenticated to save and fetch data from back-end.
 
 ### Dependencies
-Built with Bootstrap
+Built with Bootstrap and Firebase.
 
 ### Demo: [https://c-clin-angular-todo.herokuapp.com/]
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,25 +1,27 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output, OnDestroy } from '@angular/core';
 import { Response } from '@angular/http';
 import { NgForm } from '@angular/forms';
 import { AuthService } from './../auth.service';
 import { DataStorageService } from './../data.storage.service';
 import { TodoService } from '../todos/todo.services';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  title = 'Anguar Todo App';
+export class HeaderComponent implements OnInit, OnDestroy {
+  title = 'Anguar Task Manager';
   authStatus;
+  subscription: Subscription;
 
   constructor(private dataStorageService: DataStorageService,
               private authService: AuthService,
               private todoService: TodoService) { }
 
   ngOnInit() {
-    this.authService.authentication
+    this.subscription = this.authService.authentication
       .subscribe(
         (auth: boolean) => {
           this.authStatus = auth;
@@ -50,5 +52,10 @@ export class HeaderComponent implements OnInit {
 
   onFetchList() {
     this.dataStorageService.getTodos();
+  }
+
+  // unsubscribe to prevent memory leak
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
